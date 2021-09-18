c1bcd528fed26cb0a7d8fb1c22c2405de12fed0e
javedmomin99 <90521267+javedmomin99@users.noreply.github.com>
2021-09-18 14:15:58 +0530

    Add files via upload

---
+++ BMI Calculator.py
@@ -0,0 +1,20 @@
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+bmi = int(weight) / float(height) ** 2
+#print bmi in int type
+bmi_as_int = int(bmi)
+print(f"Your BMI is {bmi_as_int}")
+
+
+
+
+
+
+
+
+
+
+
+
+
